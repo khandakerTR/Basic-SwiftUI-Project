16cf10b914d6eb05f3fc38cbbafa2fd3466bb0f3
Tushar Khandaker <56843185+khandakerTR@users.noreply.github.com>
2023-08-13 01:05:22 +0600

    Initial Commit

---
+++ Basic SwiftUI Project/Basic_SwiftUI_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_SwiftUI_ProjectApp.swift
+//  Basic SwiftUI Project
+//
+//  Created by Tushar Khandaker on 13/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Basic_SwiftUI_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Basic SwiftUI Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Basic SwiftUI Project
+//
+//  Created by Tushar Khandaker on 13/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
